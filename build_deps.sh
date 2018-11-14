7a865e5b762dc6a3c7bb33c1bba97d717d9de0ca
Eduardo Gonzalez Fisher <eduardo.gonzalezfisher@kaust.edu.sa>
2018-11-14 14:11:51 +0300

    fix path in build_deps

--- build_deps.sh
+++ build_deps.sh
@@ -135,14 +135,14 @@ cd chameleon
 mkdir -p build && cd build
 cmake .. -DCHAMELEON_USE_MPI=OFF -DBUILD_SHARED_LIBS=ON -DCBLAS_DIR="${MKLROOT}" -DLAPACKE_DIR="${MKLROOT}" -DBLAS_LIBRARIES="-L${MKLROOT}/lib/intel64;-lmkl_intel_lp64;-lmkl_core;-lmkl_sequential;-lpthread;-lm;-ldl" -DCMAKE_INSTALL_PREFIX=$PREFIX
 make -j || make && make install
-cd $TMPDIR
-cd exageostatR && cd src
+#cd $TMPDIR
+cd $BASEDIR && cd src
 cd stars-h
 mkdir -p build && cd build
 cmake .. -DCMAKE_C_FLAGS=-fPIC -DMPI=OFF -DCMAKE_INSTALL_PREFIX=$PREFIX
 make -j || make && make install
-cd $TMPDIR
-cd exageostatR && cd src
+#cd $TMPDIR
+cd $BASEDIR && cd src
 cd hicma
 mkdir -p build && cd build
 cmake .. -DBUILD_SHARED_LIBS=ON -DCMAKE_INSTALL_PREFIX=$PREFIX
