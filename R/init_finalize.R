e0f5c50ce6b429c5adc2977a0415a596f05ad64a
Sameh Abdulah <sameh.abdulah@kaust.edu.sa>
2020-09-02 18:29:24 +0300

    debugging

--- R/init_finalize.R
+++ R/init_finalize.R
@@ -17,12 +17,6 @@
 
 library(assertthat)
 
-ncores <- 1 
-ngpus <- 0
-dts <- 320
-lts <- 0 
-pgrid <- 1
-qgrid <- 1
 
 #' Initial an instance of ExaGeoStatR
 #' @param hardware A list of ncores, ngpus, tile size, pgrid, and qgrid
@@ -33,19 +27,19 @@ qgrid <- 1
 #' exageostat_init(hardware = list(ncores = 26, ngpus = 0, ts = 320, lts = 0, pgrid = 3, qgrid = 4))
 exageostat_init <-
   function(hardware = list(ncores = 2, ngpus = 0, ts = 320, lts = 0, pgrid = 1, qgrid = 1)) {
-    ncores <- ifelse(is.null(hardware$ncores), 1, hardware$ncores)
-    ngpus <- ifelse(is.null(hardware$ngpus), 0, hardware$ngpus)
-    dts <- ifelse(is.null(hardware$ts), 320, hardware$ts)
-    lts <- ifelse(is.null(hardware$lts), 0, hardware$lts)
-    pgrid <- ifelse(is.null(hardware$pgrid), 1, hardware$pgrid)
-    qgrid <- ifelse(is.null(hardware$qgrid), 1, hardware$qgrid)
+    ncores <<- ifelse(is.null(hardware$ncores), 1, hardware$ncores)
+    ngpus <<- ifelse(is.null(hardware$ngpus), 0, hardware$ngpus)
+    dts <<- ifelse(is.null(hardware$ts), 320, hardware$ts)
+    lts <<- ifelse(is.null(hardware$lts), 0, hardware$lts)
+    pgrid <<- ifelse(is.null(hardware$pgrid), 1, hardware$pgrid)
+    qgrid <<- ifelse(is.null(hardware$qgrid), 1, hardware$qgrid)
 
-    assert_that(.pkgenv$ncores >= 0)
-    assert_that(.pkgenv$ngpus >= 0)
-    assert_that(.pkgenv$dts > 0)
-    assert_that(.pkgenv$lts >= 0)
-    assert_that(.pkgenv$pgrid > 0)
-    assert_that(.pkgenv$qgrid > 0)
+    assert_that(ncores >= 0)
+    assert_that(ngpus >= 0)
+    assert_that(dts > 0)
+    assert_that(lts >= 0)
+    assert_that(pgrid > 0)
+    assert_that(qgrid > 0)
 
     Sys.setenv(OMP_NUM_THREADS = 1)
     Sys.setenv(STARPU_CALIBRATE = 1)
