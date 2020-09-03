079b5058122e4d2ae2076f3b9122e8dcf62f616e
Sameh Abdulah <sameh.abdulah@kaust.edu.sa>
2020-09-03 13:01:16 +0300

    debugging

--- R/simulate_data.R
+++ R/simulate_data.R
@@ -40,30 +40,36 @@ simulate_data_exact <-
   function(sigma_sq,
            beta,
            nu,
-           dmetric = c("euclidean", "great_circle"),
+           dmetric = 0,
            n,
-           seed = 0) {
-	  
-	  dmetric <- arg_check_sim(sigma_sq, beta, nu, dmetric)
-	  assert_that(length(n) == 1)
-	  assert_that(n >= 1)
-	  assert_that(length(seed) == 1)
-	  dmetric <- as.integer(dmetric)
-	  n <- as.integer(n)
-	  seed <- as.integer(seed)
-	  globalveclen <- as.integer(3 * n)
-	  globalvec2 <- .C("gen_z_exact", sigma_sq, beta,
-			   nu, dmetric, n, seed, ncores, 
-			   ngpus, dts, pgrid, qgrid, globalveclen,
-			   globalvec = double(globalveclen)
-			   )$globalvec
-	  print("Synthetic spatial data have been generated")
-	  newList <-
-		  list(
-		 "x" = globalvec2[1:n],
-		 "y" = globalvec2[(n + 1):(2 * n)],
-		 "z" = globalvec2[((2 * n) + 1):(3 * n)]
-	    )
+           seed = 0)
+  {
+    globalveclen = 3 * n
+    globalvec  = vector (mode = "double", length = globalveclen)
+    globalvec2 = .C(
+      "gen_z_exact",
+      as.double(sigma_sq),
+      as.double(beta),
+      as.double(nu),
+      as.integer(dmetric),
+      as.integer(n),
+      as.integer(seed),
+      as.integer(ncores),
+      as.integer(ngpus),
+      as.integer(dts),
+      as.integer(pgrid),
+      as.integer(qgrid),
+      as.integer(globalveclen),
+      globalvec = double(globalveclen)
+    )$globalvec
 
+    #               globalvec[1:globalveclen] <- globalvec2[1:globalveclen]
+
+    newList <-
+      list("x" = globalvec2[1:n],
+           "y" = globalvec2[(n + 1):(2 * n)],
+           "z" = globalvec2[((2 * n) + 1):(3 * n)])
+
+    print("back from gen_z_exact  C function call. Hit key....")
     return(newList)
   }
