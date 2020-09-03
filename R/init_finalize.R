ebc083d905c733ace129202df433206495e838ac
Sameh Abdulah <sameh.abdulah@kaust.edu.sa>
2020-09-03 13:39:15 +0300

    debugging

--- R/init_finalize.R
+++ R/init_finalize.R
@@ -13,7 +13,7 @@
 # @version 1.0.1
 #
 # @author Sameh Abdulah
-# @date 2019-09-25
+# @date 2020-09-02
 
 library(assertthat)
 
@@ -26,39 +26,38 @@ library(assertthat)
 #' exageostat_init(hardware = list(ncores = 1, ngpus = 2, ts = 320, lts = 0, pgrid = 1, qgrid = 1))
 #' exageostat_init(hardware = list(ncores = 26, ngpus = 0, ts = 320, lts = 0, pgrid = 3, qgrid = 4))
 exageostat_init <-
-  function(hardware = list(ncores = 2, ngpus = 0, ts = 320, lts = 0, pgrid = 1, qgrid = 1)) {
-	  
-    if(exists("active_instance") && active_instance == 1)
-    {
-        print("There is already an active ExaGeoStatR instance.")
-        return(NULL)
-    }
-    ncores <<- ifelse(is.null(hardware$ncores), 1, hardware$ncores)
-    ngpus <<- ifelse(is.null(hardware$ngpus), 0, hardware$ngpus)
-    dts <<- ifelse(is.null(hardware$ts), 320, hardware$ts)
-    lts <<- ifelse(is.null(hardware$lts), 0, hardware$lts)
-    pgrid <<- ifelse(is.null(hardware$pgrid), 1, hardware$pgrid)
-    qgrid <<- ifelse(is.null(hardware$qgrid), 1, hardware$qgrid)
-    active_instance <<- 1
+	function(hardware = list(ncores = 2, ngpus = 0, ts = 320, lts = 0, pgrid = 1, qgrid = 1)) {
 
-    assert_that(ncores >= 0)
-    assert_that(ngpus >= 0)
-    assert_that(dts > 0)
-    assert_that(lts >= 0)
-    assert_that(pgrid > 0)
-    assert_that(qgrid > 0)
+		if(exists("active_instance") && active_instance == 1)
+		{
+			.C("rexageostat_finalize")
+		}
+		ncores <<- ifelse(is.null(hardware$ncores), 1, hardware$ncores)
+		ngpus <<- ifelse(is.null(hardware$ngpus), 0, hardware$ngpus)
+		dts <<- ifelse(is.null(hardware$ts), 320, hardware$ts)
+		lts <<- ifelse(is.null(hardware$lts), 0, hardware$lts)
+		pgrid <<- ifelse(is.null(hardware$pgrid), 1, hardware$pgrid)
+		qgrid <<- ifelse(is.null(hardware$qgrid), 1, hardware$qgrid)
+		active_instance <<- 1
 
-    Sys.setenv(OMP_NUM_THREADS = 1)
-    Sys.setenv(STARPU_CALIBRATE = 1)
-    Sys.setenv(STARPU_SILENT = 1) 
-    Sys.setenv(KMP_AFFINITY = "disabled")   
-    
-    .C("rexageostat_init",
-       as.integer(ncores),
-       as.integer(ngpus),
-       as.integer(dts))
-    print("ExaGeoStatR instance is active now.")
-  }
+		assert_that(ncores >= 0)
+		assert_that(ngpus >= 0)
+		assert_that(dts > 0)
+		assert_that(lts >= 0)
+		assert_that(pgrid > 0)
+		assert_that(qgrid > 0)
+
+		Sys.setenv(OMP_NUM_THREADS = 1)
+		Sys.setenv(STARPU_CALIBRATE = 1)
+		Sys.setenv(STARPU_SILENT = 1) 
+		Sys.setenv(KMP_AFFINITY = "disabled")   
+
+		.C("rexageostat_init",
+		   as.integer(ncores),
+		   as.integer(ngpus),
+		   as.integer(dts))
+		print("ExaGeoStatR instance is active now.")
+	}
 
 #' Finalize the current instance of ExaGeoStatR
 #' @return N/A
@@ -66,12 +65,12 @@ exageostat_init <-
 #' exageostat_finalize()
 exageostat_finalize <- function()
 {
-  if(exists("active_instance") && active_instance == 1)
-  {
-     .C("rexageostat_finalize")
-      active_instance <<- 0
-     print("ExaGeoStatR instance deactiviated.")
-  }
-  else
-     print("No active instances.")
+	if(exists("active_instance") && active_instance == 1)
+	{
+		.C("rexageostat_finalize")
+		active_instance <<- 0
+		print("ExaGeoStatR instance deactiviated.")
+	}
+	else
+		print("No active instances.")
 }
